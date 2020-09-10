b2f835ec350b94f890d806ad76a34686908c2094
jc-gi <juancarlos.gonzalez@upslp.edu.mx>
2020-09-10 18:33:06 -0500

    Revision

--- Conjuntos.rs
+++ Conjuntos.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-
+#Necesitas documentar tu codigo, asi como ejecutar las operaciones que se subieron en el archivo de python
 fn main()
 {
 	let a: HashSet<_> = [1, 2, 3,3].iter().collect();
